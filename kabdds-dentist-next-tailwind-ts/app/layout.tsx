f913835ed73027900789edbe1b314ad8c38cbb91
tbrown034 <tbrown034@gmail.com>
2023-05-10 15:58:49 -0500

    header set up

---
+++ kabdds-dentist-next-tailwind-ts/app/layout.tsx
@@ -0,0 +1,25 @@
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Navbar from "./components/layout/Navbar";
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Keith Brown DDS FAGD",
+  description:
+    "Keith Brown is a dentist serving Naperville, Auroura and the greater Chicagoland area.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
